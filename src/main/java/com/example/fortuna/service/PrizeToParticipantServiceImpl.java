35414706a5dd5d5002077f67c850610854b9b8ab
wiiiikipedia <vikakirilina2002@gmail.com>
2024-02-22 19:27:48 +0300

    unshelved

--- src/main/java/com/example/fortuna/service/PrizeToParticipantServiceImpl.java
+++ src/main/java/com/example/fortuna/service/PrizeToParticipantServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Map;
 
 @Service
 public class PrizeToParticipantServiceImpl
